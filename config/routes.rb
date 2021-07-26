cbb84682d4e6f676ccfac0232857442c6f96b622
Germán Troncoso <textosimulado@gmail.com>
2021-07-26 12:19:02 -0400

    oli

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+    get "restaurants/new", to: "restaurants#new"
+  end
+end
+
+class RestaurantsController < ApplicationController
+  def new
+    @restaurant = Restaurant.new 
+  end
 end
